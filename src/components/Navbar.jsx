734fdf2fe840da274903eb5a382c2b5c89efae18
Your Name <you@example.com>
2025-08-21 00:39:23 +0700

    header n quick fact done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from "react-scroll";
 import logo from "../assets/logo.png";
+
 
 const Navbar = () => {
     return (
@@ -7,9 +9,9 @@ const Navbar = () => {
             <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-3 lg:px-5">
                 <img src={logo} className='w-30 h-8' alt="" />
                 <ul className='hidden md:flex gap-7'>
-                    <a href="#header" className='cursor-pointer hover:text-gray-300'>Home</a>
-                    <a href="#header" className='cursor-pointer hover:text-gray-300'>Quick Fact</a>
-                    <a href="#header" className='cursor-pointer hover:text-gray-300'>People Love It</a>
+                    <Link to='header' smooth={true} duration={500} className='cursor-pointer hover:text-gray-300'>Home</Link>
+                    <Link to='quick' smooth={true} duration={500} className='cursor-pointer hover:text-gray-300'>Quick Fact</Link>
+                    <Link to='header' smooth={true} duration={500} className='cursor-pointer hover:text-gray-300'>People Love It</Link>
                 </ul>
                 <button className='hidden md:block bg-blue px-8 py-2 rounded-full'>Sign Up</button>
             </div>
