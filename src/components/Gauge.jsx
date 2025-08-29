b8d996f35c1575dd716b51905677652d58922cb5
Your Name <you@example.com>
2025-08-29 20:34:04 +0700

    gauge done

--- src/components/Gauge.jsx
+++ src/components/Gauge.jsx
@@ -5,31 +5,29 @@ const Gauge = ({ value }) => {
   const circumference = 2 * Math.PI * radius;
   const gap = 90;
 
-  const redValue = Math.min(value, 25);                        // max 25
-  const yellowValue = Math.min(Math.max(value - 25, 0), 50);   // max 25
-  const greenValue = Math.min(Math.max(value , 0), 100);    // max 50
+  const redValue = Math.min(value, 17);                        
+  const yellowValue = Math.min(Math.max(value-10 , 0), 41);   
+  const greenValue = Math.min(Math.max(value-15 , 0), 85);   
 
-  // Hitung panjang arc masing-masing
   const redProgress = (redValue / 100) * circumference;
   const yellowProgress = (yellowValue / 100) * circumference;
   const greenProgress = (greenValue  / 100) * circumference;
 
   console.log(circumference - greenProgress)
   return (
-    <div className="flex items-center justify-center bg-amber-500 w-64 h-64 relative">
+    <div className="flex items-center justify-center  w-44 h-44  relative">
       <svg
-        width="200"
-        height="200"
-        viewBox="0 0 200 200"
+        width="180"
+        height="180"
+        viewBox="0 0 240 200"
         style={{ transform: "rotate(118deg)" }}
       >
-        {/* Background arc */}
         <circle
-          cx="100"
-          cy="100"
+          cx="110"
+          cy="110"
           r={radius}
           stroke="#333"
-          strokeWidth="15"
+          strokeWidth="25"
           fill="none"
           strokeDasharray={circumference}
           strokeDashoffset={gap}
@@ -37,11 +35,11 @@ const Gauge = ({ value }) => {
         />
 
         <circle
-          cx="100"
-          cy="100"
+          cx="110"
+          cy="110"
           r={radius}
           stroke="#08CB00"
-          strokeWidth="15"
+          strokeWidth="25"
           fill="none"
           strokeLinecap="round"
           strokeDasharray={circumference}
@@ -49,11 +47,11 @@ const Gauge = ({ value }) => {
         />
 
         <circle
-          cx="100"
-          cy="100"
+          cx="110"
+          cy="110"
           r={radius}
           stroke="#FFEB00"
-          strokeWidth="15"
+          strokeWidth="25"
           fill="none"
           strokeLinecap="round"
           strokeDasharray={circumference}
@@ -61,11 +59,11 @@ const Gauge = ({ value }) => {
         />
 
         <circle
-          cx="100"
-          cy="100"
+          cx="110"
+          cy="110"
           r={radius}
           stroke="#E62727"
-          strokeWidth="15"
+          strokeWidth="25"
           fill="none"
           strokeLinecap="round"
           strokeDasharray={circumference}
@@ -74,7 +72,7 @@ const Gauge = ({ value }) => {
       </svg>
 
       <div className="absolute text-center">
-        <p className="font-bold">Index Kecocokan</p>
+        <p className="font-bold">Kecocokan</p>
         <p className="text-2xl font-extrabold">{value}%</p>
       </div>
     </div>
--- src/pages/Recom.jsx
+++ src/pages/Recom.jsx
@@ -10,9 +10,9 @@ const Recom = () => {
             <Navside />
             <div className='p-10 flex justify-between gap-10 '>
                 <Profile />
-                <div className="container bg-gray-300 p-10 min-h-screen">
+                <div className="container  p-10 min-h-screen">
                     <h1 className='font-serif text-center text-6xl' >Your Recommendation Jobs</h1>
-                    <div className="grid md:grid-cols-2 my-20">
+                    <div className="grid md:grid-cols-2 my-20 gap-10">
                         <div className="bg-white w-160 h-fit shadow-2xl p-5 rounded-4xl flex items-center gap-5 cursor-pointer">
                             <img src={company} alt="" className='size-40 rounded-4xl' />
                             <div className="bg-black/50 w-1 h-40 rounded-b-full"></div>
@@ -23,7 +23,39 @@ const Recom = () => {
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia...</p>
                             </div>
                             <Gauge value={100} />
-
+                        </div>
+                        <div className="bg-white w-160 h-fit shadow-2xl p-5 rounded-4xl flex items-center gap-5 cursor-pointer">
+                            <img src={company} alt="" className='size-40 rounded-4xl' />
+                            <div className="bg-black/50 w-1 h-40 rounded-b-full"></div>
+                            <div id="info-job" className='flex flex-col w-100 gap-2'>
+                                <h1 className='font-serif text-center mb-2 text-3xl'>JobTitle</h1>
+                                <p className='text-start'>Indofood Sukses Makmur</p>
+                                <p className='text-start'>Jakarta, Indonesia</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia...</p>
+                            </div>
+                            <Gauge value={90} />
+                        </div>
+                        <div className="bg-white w-160 h-fit shadow-2xl p-5 rounded-4xl flex items-center gap-5 cursor-pointer">
+                            <img src={company} alt="" className='size-40 rounded-4xl' />
+                            <div className="bg-black/50 w-1 h-40 rounded-b-full"></div>
+                            <div id="info-job" className='flex flex-col w-100 gap-2'>
+                                <h1 className='font-serif text-center mb-2 text-3xl'>JobTitle</h1>
+                                <p className='text-start'>Indofood Sukses Makmur</p>
+                                <p className='text-start'>Jakarta, Indonesia</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia...</p>
+                            </div>
+                            <Gauge value={100} />
+                        </div>
+                        <div className="bg-white w-160 h-fit shadow-2xl p-5 rounded-4xl flex items-center gap-5 cursor-pointer">
+                            <img src={company} alt="" className='size-40 rounded-4xl' />
+                            <div className="bg-black/50 w-1 h-40 rounded-b-full"></div>
+                            <div id="info-job" className='flex flex-col w-100 gap-2'>
+                                <h1 className='font-serif text-center mb-2 text-3xl'>JobTitle</h1>
+                                <p className='text-start'>Indofood Sukses Makmur</p>
+                                <p className='text-start'>Jakarta, Indonesia</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia...</p>
+                            </div>
+                            <Gauge value={100} />
                         </div>
 
                     </div>
